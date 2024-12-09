7bf04ed2384a6ee1f22148e831d91b6781c895a3
Weerawut <weerawut@live.com>
2024-12-09 08:52:19 +0700

    Initial Commit

---
+++ Screenable/ScreenableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenableApp.swift
+//  Screenable
+//
+//  Created by Weerawut Chaiyasomboon on 9/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ScreenableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
